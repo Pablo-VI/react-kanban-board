f5f7b9cb8b9b00dae1ed4dc9aac1b9ea088d2b7e
Pablo-VI <128979222+Pablo-VI@users.noreply.github.com>
2025-10-30 13:40:59 +0100

    refactor: Improve layout and styling of AddColumnForm component for better user experience

--- src/components/AddColumnForm.tsx
+++ src/components/AddColumnForm.tsx
@@ -19,7 +19,7 @@ export function AddColumnForm() {
     return (
       <button
         onClick={() => setIsAdding(true)}
-        className="w-72 flex-shrink-0 bg-zinc-800/50 hover:bg-zinc-800/80 text-white font-bold py-2 px-4 rounded"
+        className="w-72 flex-shrink-0 bg-zinc-800/50 hover:bg-zinc-800/80 text-white font-bold px-4 rounded h-[76px] flex items-center justify-center"
       >
         + Añadir otra columna
       </button>
@@ -27,11 +27,13 @@ export function AddColumnForm() {
   }
 
   return (
-    <div className="w-72 flex-shrink-0 bg-zinc-900 p-3 rounded-md">
+    <div className="w-72 flex-shrink-0 bg-zinc-900 p-3 rounded-md h-[112px] flex flex-col justify-between">
+      {" "}
       <input
         type="text"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
+        onKeyDown={(e) => e.key === "Enter" && handleSubmit()}
         placeholder="Introduce el título de la columna..."
         autoFocus
         className="bg-zinc-700 text-white p-2 rounded w-full mb-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
